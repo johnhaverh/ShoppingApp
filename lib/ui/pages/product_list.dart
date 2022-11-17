b17b74240ee495f4a9962e5aa91055916c2817a9
John Haver Herrera <johnhaverh@gmail.com>
2022-11-16 23:56:35 -0500

    Eliminar comentarios TODO

--- lib/ui/pages/product_list.dart
+++ lib/ui/pages/product_list.dart
@@ -75,19 +75,11 @@ class _ProductListState extends State<ProductList> {
           children: [
             IconButton(
                 onPressed: () {
-                  // TODO
-                  // aquí debemos llamar al método del controlador que
-                  // incrementa el número de unidades del producto
-                  // pasandole el product.id
                   shoppingController.agregarProducto(product.id);
                 },
                 icon: const Icon(Icons.arrow_upward)),
             IconButton(
                 onPressed: () {
-                  // TODO
-                  // aquí debemos llamar al método del controlador que
-                  // disminuye el número de unidades del producto
-                  // pasandole el product.id
                   shoppingController.quitarProducto(product.id);
                 },
                 icon: const Icon(Icons.arrow_downward))
